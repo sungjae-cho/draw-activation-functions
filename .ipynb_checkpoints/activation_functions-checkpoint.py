3c033fed997c1b9a364dcd400cc2ab6791c4e785
Sungjae <sungjae.cho.1118@gmail.com>
2018-08-23 22:02:25 +0900

    Generated new images.

---
+++ .ipynb_checkpoints/activation_functions-checkpoint.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def linear(x):
+	return x
+
+def binary_threshold(x):
+	x[x > 0] = 1
+	x[x <= 0] = 0
+	return x
+
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+def tanh(x):
+	return np.tanh(x)
+
+def relu(x):
+	return np.maximum(0, x)	
